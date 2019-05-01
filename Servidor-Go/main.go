eefbdea02247abffba41c4bcbdac3be92f0e55a8
wabgomes <wabgomes.ufrrj@gmail.com>
2019-05-01 20:32:08 -0300

    logica do servidor finalizada, mas ainda existe bug na sincronização de envio e recebimento

--- Servidor-Go/main.go
+++ Servidor-Go/main.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"fmt"
-	"./serverLogic"
-	_ "./serverConnection"
+	//"./serverLogic"
+	"./serverConnection"
 )
+/*
+func serverLogicTest(){
+	xml := "<resposta><retorno>0</retorno></resposta>"
+	xsd_path := "../Arquivos/resposta.xsd"
+
+}
+*/
+func recieveNotify(msg []byte, clinetId int) []byte {
+	var (
+		xml string
+		resposta string
+	)
+	xml = string(msg)
+	fmt.Println("\n\n======== mensagem recebida do cliente: \n", xml)
+	//resposta = serverLogic.RequestXMLHandler(xml)
+	resposta = "vc enviou yeah!"
+	fmt.Println("======== resposta a ser enviada para o cliente: \n", resposta)
+	return []byte(resposta)
+	//serverConnection.SendToClient([]byte(resposta), clinetId)
+	
+	//fmt.Println("fui notificado com : '", string(msg), "'")
+}
+
+func serverConnectionTest(){
+	fmt.Println("se registrando no observer")
+	serverConnection.RegisterObserver(recieveNotify)
+	serverConnection.OpenListener()
+}
 
 func main() {
-	//xml := "<resposta><retorno>0</retorno></resposta>"
-	//xsd_path := "Arquivos/resposta.xsd"
+	serverConnectionTest()
+/*	
+	a := 123
 
-	//serverLogic.ValidateXML(xml,xsd_path)
-	//serverLogic.CheckXML(xml)
+	b := strconv.Itoa(a)
+	b = "00000"+b
 
-	resp := serverLogic.BuildXMLResponse("2")
-	fmt.Println(resp)
+	fmt.Println(b[len(b)-5:])
+*/	
+	/*
+	serverConnection.Init()
+	serverConnection.RegisterObserver(recieveNotify)
+
+	serverConnection.Notify()
+	*/
 
 }
---
+++ Servidor-Go/serverConnection/serverConnectionWrapper.go
@@ -0,0 +1,132 @@
+package serverConnection
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"bufio"
+	"strconv"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "4446"
+	CONN_TYPE = "tcp4"
+)
+
+var (
+	observerList []func([]byte, int)[]byte
+	clinetConnList []net.Conn
+)
+
+func Init(){
+	observerList = make([]func([]byte, int)[]byte, 0)
+	clinetConnList = make([]net.Conn, 0)
+}
+
+func Restart(){
+	Init()
+}
+
+func OpenListener(){
+	Init()
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Erro listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+
+	fmt.Println("Listening em " + CONN_HOST + ":" + CONN_PORT)
+
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Conexão do cliente recebida.")
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+func readIncomingMsg(conn net.Conn, tamBuffer int) ([]byte, error){
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, tamBuffer)
+	// Read the incoming connection into the buffer.
+	reqLen, err := bufio.NewReader(conn).Read(buf) //conn.Read(buf)
+
+	if err != nil {
+		fmt.Println("Erro durante a leitura:", err.Error())
+		return nil, err
+	}
+	if reqLen < 0{
+		return []byte("-1"), nil
+	}
+	fmt.Printf("qtd de bytes lidos %d\n", reqLen)
+	return buf, nil
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) {
+	myid := len(clinetConnList)
+	clinetConnList = append(clinetConnList, conn)
+	
+	for {
+		fmt.Println("\nesperando ler tamanho")
+		reqLen, err := readIncomingMsg(conn, 5)
+		if err != nil {
+			return
+		}
+		tamMsg, err := strconv.Atoi(string(reqLen))
+		if err != nil {
+			fmt.Println("Erro durante a conversão do tamanho:", err.Error())
+			return
+		}
+		if tamMsg < 0 {
+			break
+		}
+		fmt.Printf("tamanho lido %d\n", tamMsg)
+		fmt.Println("começar a ler mensagem")
+		buf, err := readIncomingMsg(conn, tamMsg)
+		if err != nil {
+			fmt.Println("Erro durante a leitura da mensagem:", err.Error())
+			return
+		}
+
+		fmt.Println("Número de bytes lidos do cliente: ",string(reqLen))
+		fmt.Println("Array de bytes lido convertido para string:\n\n ",string(buf))
+
+		notify(buf, myid)
+		// Send a response back to person contacting us.
+		//conn.Write([]byte("Message received."))
+	}
+	// Close the connection when you're done with it.
+	clinetConnList[myid] = nil
+	conn.Close()
+}
+func RegisterObserver(recieveNotify func([]byte, int)[]byte){
+	observerList = append(observerList, recieveNotify)
+}
+
+func notify(msg[]byte, id int){
+	for _, observerCallback := range observerList {
+		SendToClient(observerCallback(msg, id), id)
+	}
+}
+
+func SendToClient(msg []byte, clinetId int){
+	conn := clinetConnList[clinetId]
+
+	tamMsg := strconv.Itoa(len(msg))
+	tamMsg = "00000" + tamMsg
+
+	conn.Write([]byte(tamMsg[len(tamMsg) - 5:]))
+	conn.Write(msg)
+	
+	return 
+}
