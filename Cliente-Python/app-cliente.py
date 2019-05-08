be3d30ed9f359e11069e7a09b12f13e44f91f716
Vitor <vitorstormerss@gmail.com>
2019-05-08 18:42:21 -0300

    beta 1
    -main completo
    -chamada de métodos corrigida
    -algumas coisas pequenas mudaram

--- Cliente-Python/app-cliente.py
+++ Cliente-Python/app-cliente.py
@@ -9,7 +9,6 @@ from socket import *
 from threading import *
 
 import time
-import os
 
 def_cod = 'utf-8'
 TESTE = False
@@ -36,10 +35,13 @@ class ClientSocket(Thread):
         try:
             addrsFileLines = open("addrs.txt", "r").readlines()
         except Exception as e:
-            print(e)
+            host = 'localhost'
+            port = '4446'
+            print(str(e) + "Creating a file with default host:\'" + host + "\', port:" + port)
+            addrs = open("addrs.txt", "w+")
+            addrs.write(host+"\n"+port)
         else:
             self.host, self.port = addrsFileLines[0][:-1], int(addrsFileLines[1]) # host e port padrôes lidos do arquivo
-
 
     def run(self):
         # Método que implementa o que a Thread roda
@@ -64,12 +66,22 @@ class ClientSocket(Thread):
             print(e)
             return False
 
-    def proto_define_connect(self):
-        if self.connect():
-            self.sock.send(str(self._hdr_num).encode("utf-8"))
-            return True
-        else:
+    def connect_pd(self):
+        while not self.connect():
+            ans = input(
+                "_______________________________\n"
+                "[ENTER] - Tentar conectar novamente\n"
+                "0 - Sair do programa\n"
+                "_______________________________\n")
+            print()
+
+            if ans == '0':
+                exit(0)
+
             return False
+        else:
+            self.sock.send(str(self._hdr_num).encode(def_cod))
+            return True
 
     def send(self, msg):
         ''' Método para envio de mensagens'''
@@ -167,7 +179,7 @@ class ClientSimple(ClientSocket):
         elif len(hdr_len) > self.pHdr_len:
             raise RuntimeError("Proto Cabeçalho maior que " + str(self.pHdr_len) + " bytes")
 
-        data = (hdr_len[-self.pHdr_len:] + msg).encode("utf-8")
+        data = (hdr_len[-self.pHdr_len:] + msg).encode(def_cod)
         total_len = len(data)
         totalsent = 0
 
@@ -199,37 +211,93 @@ class ClientSimple(ClientSocket):
         return msg
 
 class Candidato():
-    def __init__(self, cpf):
+    def _init(self, cpf):
         self.cpf = cpf
-        self.boletim = 'boletins\\' + self.cpf + '.xml'
-        self.menssagem = ClientSocket()
-        self.menssagem.proto_define_connect()
+        self.boletim = open('boletins\\' + cpf + '.xml', 'r+', encoding=def_cod).read()
+        self.mensageiro = ClientSocket()
+        self.mensageiro.connect_pd()
 
     def submeter(self):
         ctrlXML = ControladorXML()
+        ctrlXML.lerXSD("resposta")
 
-        boletim = ctrlXML.lerXML(self.boletim)
-        msg = ctrlXML.criarRequisicao("submeter", {'Boletim': ctrlXML.to_string(boletim).decode(def_cod)})
-        self.menssagem.send(ctrlXML.to_string(msg).decode(def_cod))
+        msg = ctrlXML.criarRequisicao("submeter", {'Boletim': self.boletim})
+        self.mensageiro.send(msg)
+        resp = self.mensageiro.read()
+        xml_resp = ctrlXML.toXML(resp)
+
+        try:
+            if not ctrlXML.validarXML(xml_resp):
+                print("XML de resposta inválido")
+        except Exception as e:
+            print(e)
+
+        else:
+            resp = xml_resp.getroot().find('resposta').find('retorno').text
+            if resp == '0':
+                print("sucesso")
+            elif resp == '1':
+                print("XML inválido")
+            elif resp == '2':
+                print("XML mal-formado")
+            elif resp == '3':
+                print("Erro Interno")
 
     def consultaStatus(self):
-        pass
+        ctrlXML = ControladorXML()
+        ctrlXML.lerXSD("resposta")
+
+        msg = ctrlXML.criarRequisicao("consultaStatus", {'cpf': self.cpf})
+        self.mensageiro.send(msg)
+        resp = self.mensageiro.read()
+        xml_resp = ctrlXML.toXML(resp)
+
+        try:
+            if not ctrlXML.validarXML(xml_resp):
+                print("XML de resposta inválido")
+        except Exception as e:
+            print(e)
+
+        else:
+            resp = xml_resp.getroot().find('resposta').find('retorno').text
+            if resp == '0':
+                print("Candidato não encontrado")
+            elif resp == '1':
+                print("Em processamento")
+            elif resp == '2':
+                print("Candidato Aprovado e Selecionado")
+            elif resp == '3':
+                print("Candidato Aprovado e em Espera")
+            elif resp == '4':
+                print("Candidato Não Aprovado")
+
+    def identificarCandidato(cls):
+        cpf = input("Digite o CPF:")
+        print()
+        try:
+            c = Candidato()
+            c._init(cpf)
+        except FileNotFoundError:
+            print("CPF informado é inválido")
+            return None
+        else:
+            return c
 
 class ControladorXML():
     # Caso seja necessário mais alguma funcionalidade que lide com XML/XSD implementar nesta classe
-    def lerXML(self, nome_arq):
+    def toXML(self, nome):
         # Abrindo o arquivo xml
         try:
-            xml_arq = open(nome_arq, "r+", encoding=def_cod)
-        except Exception as e:
-            print(e)
-            return None
+            str_xml = open(nome, "r+", encoding=def_cod).read()
+        except FileNotFoundError:
+            str_xml = nome
+
         # Transformando o arquivo aberto em arvore de elementos
-        return etree.parse(StringIO(xml_arq.read()))
+        return etree.parse(StringIO(str_xml))
 
     def lerXSD(self, nome_arq):
         # Abrindo o arquivo xsd
-        xsd_arq = open(nome_arq, "r+", encoding=def_cod)
+        xsd_arq = open("schemas\\"+nome_arq+".xsd", "r+", encoding=def_cod)
 
         # Transformando o arquivo aberto em arvore de elementos
         xsd_doc = etree.parse(xsd_arq)
@@ -238,9 +306,6 @@ class ControladorXML():
         self.xsd =  etree.XMLSchema(xsd_doc)
         return self.xsd
 
-    def gravarXML(self):
-        pass
-
     def validarXML(self, xml):
         if self.xsd.validate(xml):
             return True
@@ -248,7 +313,7 @@ class ControladorXML():
             return False
 
     def to_string(self, xml):
-        return etree.tostring(xml, encoding=def_cod)
+        return etree.tostring(xml, encoding=def_cod).decode(def_cod)
 
     def criarRequisicao(self, nome_func, dict_param):
         root = etree.Element("requisicao")
@@ -260,154 +325,7 @@ class ControladorXML():
             etree.SubElement(parametro, "nome").text = nome
             etree.SubElement(parametro, "valor").text = etree.CDATA(valor)
 
-        return root
-    
-    def imprimir(XMLdoHistorico): #parametro: string
-    
-        #xml_arq = open(XMLdoHistorico, "r",-1,"utf-8")
-        xml_arq = etree.parse(StringIO(XMLdoHistorico))
-        xml = xml_arq.getroot()
-
-        print("------------------------------------------------------------------------")
-        print(xml.find('universidade').find('nome').text)
-        print(xml.find('universidade').find('abreviacao').text)
-        print("Curso: "+xml.find('curso').text)
-        print("Aluno: "+xml.find('aluno').text)
-        print("Matricula: "+xml.find('matricula').text)
-        print("Cr medio: "+xml.find('crMedio').text)
-        print("Data geracao: "+xml.find('dataGeracao').text)
-        print("Hora geracao: "+xml.find('horaGeracao').text)
-        print("Cod autenticacao: "+xml.find('codigoAutenticacao').text)
-        print("------------------------------------------------------------------------")
-        print("\n")
-        listaPeriodos = xml.find('periodos').findall('Periodo')
-        for i in range(len(listaPeriodos)):
-            print("Ano Semestre: "+listaPeriodos[i].find('anoSemestre').text)
-            print("Creditos solicitados: "+listaPeriodos[i].find('creditosSolicitados').text)
-            print("Creditos acumulados: "+listaPeriodos[i].find('creditosAcumulados').text)
-            print("Creditos obtidos: "+listaPeriodos[i].find('creditosObtidos').text)
-            print("Cr periodo: "+listaPeriodos[i].find('crPeriodo').text)
-            print("\n")
-
-            listaDisciplinasAA = listaPeriodos[i].find('disciplinas').findall('AtividadeAcademica')
-            for j in range(len(listaDisciplinasAA)):
-                print("\tCodigo disciplina: "+listaDisciplinasAA[j].find('codigo').text)
-                print("\tNome disciplina: "+listaDisciplinasAA[j].find('nome').text)
-                print("\tCreditos: "+listaDisciplinasAA[j].find('creditos').text)
-                print("\tNota: "+listaDisciplinasAA[j].find('nota').text)
-                print("\tSituacao: "+listaDisciplinasAA[j].find('situacaoAA').text)
-                print("\n")
-
-            listaDisciplinas = listaPeriodos[i].find('disciplinas').findall('Disciplina')
-            for j in range(len(listaDisciplinas)):
-                print("\tCodigo disciplina: "+listaDisciplinas[j].find('codigo').text)
-                print("\tNome disciplina: "+listaDisciplinas[j].find('nome').text)
-                print("\tCreditos: "+listaDisciplinas[j].find('creditos').text)
-                print("\tNota: "+listaDisciplinas[j].find('nota').text)
-                print("\tSituacao: "+listaDisciplinas[j].find('situacao').text)
-                print("\n")
-
-            print("------------------------------------------------------------------------")
-
-
-    def geraHtml(XMLdoHistorico): #parametro: string
-
-        #xml_arq = open(XMLdoHistorico, "r",-1,"utf-8")
-        xml_arq = etree.parse(StringIO(XMLdoHistorico))
-        xml = xml_arq.getroot()
-        texto = []
-        name = "historico"
-        name = name + '.html'
-        arq = open(name, 'w',-1,"utf-8")
-
-        print('Gerando',name,'...\n')
-
-
-        texto.append("<!DOCTYPE html>\n<html lang='pt-BR'>\n\n<html>\n\n")
-        texto.append("\t<head>\n\t\t<title>Histórico</title>\n\t\t<meta charset = 'utf-8'>\n\t\t<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">\n"+"\t\t<link rel=\"shortcut icon\" href=\"ufrrj.jpg\" type=\"image/jpg\"/>\n\t</head>\n")
-        texto.append("\n\t<body>\n")
-        texto.append("\n\t\t<header>\n")
-        texto.append("\t\t<a href=\"http://portal.ufrrj.br\" title=\"UFRRJ\"><img src=\"ufrrj.jpg\" class = \"imagem\" align = \"left\" alt=\"Falha na imagem\"></a>\n")
-        texto.append("\t\t<h1><br>"+xml.find('universidade').find('nome').text+"</h1>\n")
-        texto.append("\t\t<h1>"+xml.find('universidade').find('abreviacao').text+"</h1><br><br>\n")
-        texto.append("\t\t</header>\n\n")
-        texto.append("\n\t\t<div>\n")
-        texto.append("\t\t<img src=\"perfil.jpg\" class = \"imagemPerfil\" align = \"left\" title=\"Perfil\" alt=\"Falha na imagem\"><br>Curso: "+xml.find('curso').text+"<br>\n")
-        texto.append("\t\tAluno: "+xml.find('aluno').text+"<br>\n")
-        texto.append("\t\tMatrícula: "+xml.find('matricula').text+"<br>\n")
-        texto.append("\t\tCR médio: "+xml.find('crMedio').text+"<br>\n")
-        texto.append("\t\tData geração: "+xml.find('dataGeracao').text+"<br>\n")
-        texto.append("\t\tHora geração: "+xml.find('horaGeracao').text+"<br>\n")
-        texto.append("\t\tCód. autenticação: "+xml.find('codigoAutenticacao').text+"<br><br>\n")
-        texto.append("\t\t</div>\n")
-
-
-        texto.append("\n\t\t<section>\n")
-        listaPeriodos = xml.find('periodos').findall('Periodo')
-        texto.append("\t\t\t<img src=\"legenda.png\" class = \"imagemLegenda\" align = \"right\" title=\"legenda\" alt=\"Falha na imagem\">\n")
-        for i in range(len(listaPeriodos)):
-            texto.append("\t\t\t<br><br><table>\n")#\n\t\t\t<tr><th>Ano Semestre</th><th>Creditos solicitados</th><th>Creditos acumulados</th><th>Creditos obtidos</th><th>Cr periodo</th></tr><br>\n")
-            texto.append("\t\t\t<tr><th>"+listaPeriodos[i].find('anoSemestre').text+"</th></tr>\n")
-
-            texto.append("\t\t\t\t<tr><th>Código disciplina</th>"+"<th>Nome disciplina</th>"+"<th>Créditos</th>"+"<th>Nota</th>"+"<th>Situação</th></tr>\n")
-
-            listaDisciplinasAA = listaPeriodos[i].find('disciplinas').findall('AtividadeAcademica')
-            for j in range(len(listaDisciplinasAA)):
-                texto.append("\t\t\t\t<tr><td>"+listaDisciplinasAA[j].find('codigo').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('nome').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('creditos').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('nota').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('situacaoAA').text+"</td></tr>\n\n")
-
-
-            listaDisciplinas = listaPeriodos[i].find('disciplinas').findall('Disciplina')
-            for j in range(len(listaDisciplinas)):
-                texto.append("\t\t\t\t<tr><td>"+listaDisciplinas[j].find('codigo').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('nome').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('creditos').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('nota').text+"</td>\n")
-                texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('situacao').text+"</td></tr>\n\n")
-
-            texto.append("\t\t\t</tr><td></td><td>Créditos solicitados: "+listaPeriodos[i].find('creditosSolicitados').text+"</td>\n")
-            texto.append("\t\t\t<td>Créditos acumulados: "+listaPeriodos[i].find('creditosAcumulados').text+"</td>\n")
-            texto.append("\t\t\t<td>Créditos obtidos: "+listaPeriodos[i].find('creditosObtidos').text+"</td>\n")
-            texto.append("\t\t\t<td>CR período: "+listaPeriodos[i].find('crPeriodo').text+"</td></tr>\n\n")
-            texto.append("\t\t\t</table>\n\n")
-
-        texto.append("\n\t\t</section>\n")
-        texto.append("\n\t\t<footer>\n")
-        texto.append("\t\t\t<br>Grupinho de TEDB, 2019.")
-        texto.append("\n\t\t</footer>\n")
-        texto.append("\t</body>\n")
-        texto.append("\n</html>")
-
-
-
-
-        arq.writelines(texto)
-
-        print(name,"gerado com sucesso.\nSalvo em:", os.path.abspath(name))
-
-        arq.close()
-
-        flag = False
-
-        while(flag == False):
-
-            option = input("\nDeseja abrir o historico gerado? (S / N)\n")
-
-            if(option == 's' or option == 'S' or option == 'sim' or option == 'yes' or option == 'y'):
-                os.startfile(os.path.abspath(name))
-                flag = True
-
-            elif(option == 'n' or option == 'N' or option == 'nao' or option == 'not' or option == 'no'):
-                flag = True
-
-            if(flag == False):
-                print("Opção inválida. Tente novamente...")
-
-        print("Visualização encerrada.")
-    
+        return self.to_string(root)
 
     def imprimir(XMLdoHistorico):  # parametro: string
 
@@ -559,67 +477,47 @@ class ControladorXML():
 
 def main():
     ''' Função para execução no terminal'''
-
-    # no caso de implementação do menu isso deverá ser alterado
-    #candidato = Candidato(input("digite o cpf do candidato a entrar:"))
-
-    # client = ClientSocket()
-    # client.connect()
-
-    def login():
-        while True:
-            ans = input(
-                "_______________________________"
-                "1 - Fazer Login"
-                "0 - Fechar Aplicação"
-                "_______________________________")
-            if ans == '1':
-                cpf = input("Digite o CPF do Candidato:")
-            elif ans == '0':
-                print("Programa encerrado")
-                exit(0)
-
-    #candidato = Candidato(input())
+    c = None
 
     while True:
         ans = input(
-        "_______________________________"
-        "1 - submeter boletim"
-        "2 - consultar status"
-        "3 - entrar como outro candidato"
-        "0 - sair do programa"
-        "_______________________________")
+        "_______________________________\n"
+        "1 - Fazer Login\n"
+        "2 - Submeter boletim\n"
+        "3 - Consultar status\n"
+        "4 - Fazer Logoff\n"
+        "0 - Sair do programa\n"
+        "_______________________________\n\n")
+        print()
 
         if ans == '1':
-            # candidato.submeter()
-
-            pass
+            c = Candidato().identificarCandidato()
         elif ans == '2':
-            pass
+            if c is None:
+                print("Comando inválido, faça Login primeiro")
+            else:
+                c.submeter()
         elif ans == '3':
-            pass
+            if c is None:
+                print("Comando inválido, faça Login primeiro")
+            else:
+                c.consultaStatus()
+        elif ans == '4':
+            if c is None:
+                print("Comando inválido, faça Login primeiro")
+            else:
+                c.mensageiro.close_con()
+                c = None
         elif ans == '0':
+            c.mensageiro.close_con()
             break
         else:
             print("Entrada não esperada")
-    # possível fazer uma menu do tipo:
-    # 1 - submeter boletim
-    # 2 - consultar status
-    # 3 - entrar como outro candidato
-    # 0 - sair do programa
-
-    # perguntar se candidato gostaria de submeter boletim
-    #candidato.submeter()
-
-    # perguntar se candidato gostaria de consultar status
-    # candidato.consultaStatus()
-
-    pass
 
 def teste():
     '''Operações de teste'''
-    c = Candidato("00000000002")
-    c.submeter()
+    ctrl = ControladorXML()
+
     # ctrlXML = ControladorXML()
     #
     # xml = ctrlXML.criarRequisicao("consultaStatus", {'cpf': '0001'})
@@ -661,4 +559,4 @@ def teste():
                 waiting_HE = False
 '''
 
-teste()
+main()
